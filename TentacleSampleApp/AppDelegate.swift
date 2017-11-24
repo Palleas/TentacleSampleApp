d8f791347dadafe811d0c1a635f92259ca6ae618
Romain Pouclet <romain.pouclet@gmail.com>
2017-11-24 09:32:50 -0500

    Fetch branches from repo

--- TentacleSampleApp/AppDelegate.swift
+++ TentacleSampleApp/AppDelegate.swift
@@ -7,15 +7,22 @@
 //
 
 import Cocoa
+import Tentacle
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
 
     @IBOutlet weak var window: NSWindow!
 
-
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Insert code here to initialize your application
+        let token = ProcessInfo.processInfo.environment["GITHUB_TOKEN"]!
+        let repo = Repository(owner: "Palleas", name: "CommitMessageEditor")
+
+        let client = Client(.dotCom, token: token)
+        client
+            .execute(repo.branches)
+            .logEvents()
+            .start()
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
