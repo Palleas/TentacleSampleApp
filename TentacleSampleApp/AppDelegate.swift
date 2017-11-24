c7389ab0572669e7cf7ddb1382625284bea54c5e
Romain Pouclet <romain.pouclet@gmail.com>
2017-11-24 09:28:14 -0500

    Initial Commit

---
+++ TentacleSampleApp/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  TentacleSampleApp
+//
+//  Created by Romain Pouclet on 2017-11-24.
+//  Copyright © 2017 Perfectly-Cooked. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
